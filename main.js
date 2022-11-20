3bd253fbb750879900b938f94b16fb3f8a353a9d
nitinrathod.tg <nitinrathod.tg@gmail.com>
2022-11-20 16:27:14 +0530

    done new update

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// alert("Welcome To Learn With Nitin");
+
+var courseList = document.getElementById("courseList");
+var mainScreen = document.getElementById("mainScreen");
+
+courseList.addEventListener('click', function (e) {
+    var item = e.target.getAttribute('data-src');
+    e.target.classList.add("activeThis");
+
+    mainScreen.src = item;
+
+
+});
