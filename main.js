bd5129bbb55c9d49708fe0613412acf86d510cdc
nitinrathod.tg <nitinrathod.tg@gmail.com>
2022-11-25 01:56:23 +0530

    done with changes todo complete

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ var mainScreen = document.getElementById("mainScreen");
 // todo list variables 
 var taskInput =  document.getElementById("input-task");
 var addTaskBtn =  document.getElementById("add-task");
+var saveTaskBtn =  document.getElementById("save-task");
 var taskListBox =  document.getElementById("show-task-box");
+let saveIndex = document.getElementById('index');
+let deleteAll = document.getElementById('delete-all');
 
 courseList.addEventListener('click', function (e) {
     var item = e.target.getAttribute('data-src');
@@ -29,21 +32,23 @@ addTaskBtn.addEventListener("click", function(){
         taskObj = JSON.parse(taskList);
     }
 
-    if (!taskInputText == "") {
+    if (taskInputText.trim()!=0) {
         taskObj.push(taskInputText);
         localStorage.setItem("savedTask", JSON.stringify(taskObj));
-        showTask();
         
     }
     else{
         alert("Please Add Something in task !");
     }
-
+    taskInput.value = '';
+    
+    showTask();
     
 })
 
-function showTask() {
 
+// show task function 
+function showTask() {
     let taskList = localStorage.getItem("savedTask");
     if (taskList == null){
         taskObj= [];        
@@ -56,20 +61,59 @@ function showTask() {
         taskTemplate += `<div class="show-task d-flex justify-content-start align-content-start">
                             <span>${index+1}</span>
                             <p>${item}</p>
-                            <button class="update task-btn"><i class="fa-duotone fa fas fa-pen-to-square"></i></button>
-                            <button class="delete task-btn"><i class="fa-solid fa fas fa-delete-left"></i></button>
+                            <button class="update task-btn" title="Edit Task" onclick="edittask(${index})"><i class="fa-duotone fa fas fa-pen-to-square"></i></button>
+                            <button class="delete task-btn" title="Delete Task" onclick="deletetask(${index})"><i class="fa-solid fa fas fa-delete-left"></i></button>
                         </div>`
     });
-    taskListBox.innerHTML = taskTemplate;
-
-
-
-
-
-
-
-
-
-
-    
+    taskListBox.innerHTML = taskTemplate;    
 }
+
+// edit task function 
+function edittask(index){
+    let taskList = localStorage.getItem("savedTask");
+    let taskObj = JSON.parse(taskList);
+    taskInput.value = taskObj[index]
+    saveIndex.value = index;
+    addTaskBtn.style.display="none";
+    saveTaskBtn.style.display="block";
+}
+
+// save edited task function 
+saveTaskBtn.addEventListener("click", function(){
+    let taskList = localStorage.getItem("savedTask");
+    let taskObj = JSON.parse(taskList);
+    let indexValue =  saveIndex.value;
+    taskObj[indexValue] = taskInput.value;
+    localStorage.setItem("savedTask", JSON.stringify(taskObj));
+    showTask();
+    addTaskBtn.style.display="block";
+    saveTaskBtn.style.display="none";
+    taskInput.value = '';
+})
+
+// delete task function 
+function deletetask(index){
+    let taskList = localStorage.getItem("savedTask");
+    let taskObj = JSON.parse(taskList);
+    taskObj.splice(index,1);
+    localStorage.setItem("savedTask", JSON.stringify(taskObj));
+    showTask();
+
+}
+
+// delete all function 
+deleteAll.addEventListener("click", function(){
+    let taskList = localStorage.getItem("savedTask");
+    let taskObj = JSON.parse(taskList);
+    
+    if (taskList == null){
+        taskObj= [];        
+    }
+    else{
+        taskObj = JSON.parse(taskList);
+        taskObj= []; 
+    }
+    localStorage.setItem("savedTask", JSON.stringify(taskObj));
+    showTask();
+
+});
