04c53539397ba7556533626e79e6942863d40597
nitinrathod.tg <nitinrathod.tg@gmail.com>
2022-11-26 18:04:35 +0530

    done

---
+++ courses.js
@@ -0,0 +1,58 @@
+// all courses script here API json fetching.
+
+var javascript = document.getElementById('javascript-courses');
+var javascriptFile = 'courses/javascript.json';
+
+var html = document.getElementById('html-courses');
+var htmlFile = 'courses/javascript.json';
+
+var css = document.getElementById('css-courses');
+var cssFile = 'courses/javascript.json';
+
+var jquery = document.getElementById('jquery-courses');
+var jqueryFile = 'courses/javascript.json';
+
+
+
+
+function getAllCourses(container, filename,) {
+    let http = new XMLHttpRequest();
+    http.open('get', `${filename}`, true);
+    http.send();
+    http.onload = function () {
+
+        if (this.readyState == 4 && this.status == 200){
+            let courseData = JSON.parse(this.responseText);
+            let output = '';
+
+            for (let item of courseData) {
+                output += `                    
+                        <div class="other-course-box detail-course-box">
+                                    <div class=" col-12">
+                                        <div class="other-video-secreen">
+    
+                                            <iframe width="560" height="315" src="${item.iframe}"
+                                                title="YouTube video player" frameborder="0"
+                                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                                allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+    
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <h2 class="other-course-headline">${item.title}</h2>
+                                        </div>
+                                    </div>
+                                </div>                 
+                            `;
+            }
+
+            container.innerHTML = output;
+        }
+    }
+}
+
+getAllCourses(javascript, javascriptFile);
+getAllCourses(html, htmlFile);
+getAllCourses(css, cssFile);
+getAllCourses(jquery, jqueryFile);
