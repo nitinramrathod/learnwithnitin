44b7798cfdf8283b7cb13fbd859cc228ff655f6a
nitinrathod.tg <nitinrathod.tg@gmail.com>
2022-11-27 21:04:10 +0530

    smily added

--- main.js
+++ main.js
@@ -13,10 +13,25 @@ let cursor = document.getElementById('pointer');
 
 
 // mouse pointer 
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
-})
+// document.addEventListener('mousemove', (e) => {
+//     cursor.style.left = e.pageX + 'px';
+//     cursor.style.top = e.pageY + 'px';
+// })
+
+document.querySelector('body').addEventListener('mousemove', eyeball);
+// making function here 
+function eyeball(){
+    var eye = document.querySelectorAll('.eye');
+
+    eye.forEach(function(eye){
+        let x = (eye.getBoundingClientRect().left)+ (eye.clientWidth/2);
+        let y = (eye.getBoundingClientRect().top)+ (eye.clientHeight/2);
+
+        let radian = Math.atan2(event.pageX -x, event.pageY- y);
+        let rot = (radian * (180/Math.PI) * -1)+ 230;
+        eye.style.transform = "rotate("+rot+"deg)";
+    })
+}
 
 
 
